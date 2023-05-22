69fdd336ca55bf204a9dd6a2c9225f809fd08ef6
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-23 00:16:57 +0100

    pass date child->parent and bind

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
 const [person,setPerson]=useState([
   {name:'usf',age:24,metier:'full stack'},{name:'kamal',age:29,metier:'ATM'}
 ]);
-  const updatePerson=()=>{
+  const updatePerson=(good)=>{
     const updPerson=[...person];
-    updPerson[0].name='youssef TADLAOUI'
+    updPerson[0].name=good
     setPerson(updPerson);
   }
   const [namePerson,updateName]=useState('');
@@ -28,8 +28,8 @@ const [person,setPerson]=useState([
   return (
     <div className="App">
       <Youssef name={person[0].name} age={person[0].age} metier={person[0].metier} />
-      <Youssef name={person[1].name} age={person[1].age} metier={person[1].metier} >ok im here</Youssef>
-      <button onClick={updatePerson}>Mettre à jour le nom</button>
+      <Youssef click={updatePerson.bind(this,'nada')} name={person[1].name} age={person[1].age} metier={person[1].metier} >ok im here</Youssef>
+      <button onClick={updatePerson.bind(this,'tadlaoui')}>Mettre à jour le nom</button>
       <form onSubmit={handlSubmit}>
   <label>
     <input type='text' onChange={handlName}></input>
--- src/components/Youssef.js
+++ src/components/Youssef.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Youssef(props){
-  const  {name,age,metier} = props;
+  const  {name,age,metier,click} = props;
   const grandName={
     color:'blue',
     fontWeight:'bolde',
@@ -10,7 +10,7 @@ function Youssef(props){
 return(
     <div>
       <h1 style={grandName}>{name}</h1>
-      <p>age:{age},{metier}</p>
+      <p onClick={click} >age:{age},{metier}</p>
       <p>{props.children}</p>
     </div>
  );
