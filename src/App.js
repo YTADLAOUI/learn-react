e792dfdefd0d700d95da212d00133e85dc176e0f
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-23 12:44:33 +0100

    clean code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import Youssef from'./components/Youssef.js'
+import Fin from'./components/Youssef.js'
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-const [person,setPerson]=useState([
-  {name:'usf',age:24,metier:'full stack'},{name:'kamal',age:29,metier:'ATM'}
-]);
-  const updatePerson=(good)=>{
-    const updPerson=[...person];
-    updPerson[0].name=good
-    setPerson(updPerson);
-  }
-  const [namePerson,updateName]=useState('');
-  // console.log(namePerson,'hh')
-  // console.log(person);
-  const UpdtName=()=>{
-    const updPerson=[...person];
-    updPerson[0].name=namePerson;
-    setPerson(updPerson)
-  }
- const handlName =(event)=>{
-  updateName(event.target.value)
- }
- const handlSubmit=(event)=>{
-    event.preventDefault();
-    UpdtName();
-    console.log(namePerson,'hh')
- }
+
   return (
     <div className="App">
-      
-      <Youssef name={person[0].name} age={person[0].age} metier={person[0].metier} />
-      <Youssef click={updatePerson.bind(this,'nada')} name={person[1].name} age={person[1].age} metier={person[1].metier} >ok im here</Youssef>
-      <button onClick={updatePerson.bind(this,'tadlaoui')}>Mettre à jour le nom</button>
-      <form onSubmit={handlSubmit}>
-  <label>
-    <input type='text' onChange={handlName}></input>
-    <button type='submit'>change </button>
-  </label>
-
-      </form>
-      
+      <Fin/>
     </div>
   );
 }
--- src/components/Youssef.js
+++ src/components/Youssef.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './style.css'
 
+ 
 
 
 
@@ -24,4 +25,52 @@ return(
     </div>
  );
 }
-export default Youssef;
+
+
+function fin(){
+  const [person,setPerson]=useState([
+    {name:'usf',age:24,metier:'full stack'},{name:'kamal',age:29,metier:'ATM'}
+  ]);
+    const updatePerson=(good)=>{
+      const updPerson=[...person];
+      updPerson[0].name=good
+      setPerson(updPerson);
+    }
+    const [namePerson,updateName]=useState('');
+    // console.log(namePerson,'hh')
+    // console.log(person);
+    const UpdtName=()=>{
+      const updPerson=[...person];
+      updPerson[0].name=namePerson;
+      setPerson(updPerson)
+    }
+   const handlName =(event)=>{
+    updateName(event.target.value)
+   }
+   const handlSubmit=(event)=>{
+      event.preventDefault();
+      UpdtName();
+      console.log(namePerson,'hh')
+   }
+   return (
+    <div>
+
+      {/* {person.map((person)=>{
+        return<Youssef name={person.name} age={person.age} metier={person.metier} />
+      })} */}
+
+      <Youssef name={person[0].name} age={person[0].age} metier={person[0].metier} />
+      <Youssef click={updatePerson.bind(this,'nada')} name={person[1].name} age={person[1].age} metier={person[1].metier} >ok im here</Youssef>
+      <button onClick={updatePerson.bind(this,'tadlaoui')}>Mettre à jour le nom</button>
+      <form onSubmit={handlSubmit}>
+  <label>
+    <input type='text' onChange={handlName}></input>
+    <button type='submit'>change </button>
+  </label>
+
+      </form>
+      
+    </div>
+  );
+}
+export default fin;
