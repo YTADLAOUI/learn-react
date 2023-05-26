ef18a56980d3e019c9b134555ef6e37afc8d2251
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-27 00:42:13 +0100

    count

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import { useState } from 'react';
 
 function App() {
 
-
+const [count,setCount]=useState(0);
+const inctrmment=()=>{
+setCount(count + 1)
+}
+const decrement=()=>{
+  count > 0 ?
+setCount(count - 1) :setCount(count)
+}
   return (
     <div className="App">
-      <Fin/>
-      <Test/>
+      {/* <Fin/>
+      <Test/> */}
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={inctrmment}>+</button>
     </div>
   );
 }
